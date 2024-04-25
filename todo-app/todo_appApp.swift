d58233345c1502b7cea934ebee2697bd137c99b4
MaevaWolff <maeva.wolff@hetic.net>
2024-04-26 01:20:17 +0200

    Initial Commit

---
+++ todo-app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo-app
+//
+//  Created by Maxence Wolff on 26/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
