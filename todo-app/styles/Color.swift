c12a02ae264879418cbca572fadaf6558668530e
MaevaWolff <maeva.wolff@hetic.net>
2024-05-14 07:06:58 +0200

    feat: add custom colors

---
+++ todo-app/styles/Color.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+extension Color {
+    // Elements
+    public static let background: Color = Color(UIColor(red: 32/255, green: 32/255, blue: 32/255, alpha: 1.0))
+    public static let outline: Color = Color(UIColor(red: 55/255, green: 55/255, blue: 55/255, alpha: 1.0))
+    public static let text: Color = Color(UIColor(red: 186/255, green: 186/255, blue: 186/255, alpha: 1.0))
+    
+    // Brand
+    public static let one: Color = Color(UIColor(red: 98/255, green: 110/255, blue: 217/255, alpha: 1.0))
+    public static let two: Color = Color(UIColor(red: 236/255, green: 187/255, blue: 91/255, alpha: 1.0))
+    
+    // Utils
+    public static let danger: Color = Color(UIColor(red: 198/255, green: 68/255, blue: 68/255, alpha: 1.0))
+}
